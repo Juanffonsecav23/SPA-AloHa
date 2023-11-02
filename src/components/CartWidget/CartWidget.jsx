c85224e955e2bc2a93a0f4977d52dbe8ad4b5a71
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2023-11-02 17:04:07 -0500

    Finalizacion del proyecto

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { cartContext } from "../../context/cartContext"
+import { cartContext } from "../../context/CartContext"
 import { Link } from "react-router-dom"
 
 export default function CarWidget (){
