fceda60d7298fe2299a2da65e170126a0c409619
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2024-02-02 12:52:10 -0500

    Se cambio la fuente de la agina en el navbar y tambien se corrigio el mapa que estaba amuy pequeño

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -27,7 +27,7 @@ function Contacto() {
         <h1 style={{color:"orange"}}>Contacto</h1>
         <BotonosRedes/>
         <CheckOutForm/>
-        <div className="mapa"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15904.82915919686!2d-74.0427604!3d4.7340203!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6e54073e5ae47ebb%3A0x6860988c008d3b3!2sCasa%20AloHa%20152%20Alojamiento%20Habitacional!5e0!3m2!1sen!2sco!4v1698453773716!5m2!1sen!2sco" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe></div>
+        <div className="mapa"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15904.82915919686!2d-74.0427604!3d4.7340203!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6e54073e5ae47ebb%3A0x6860988c008d3b3!2sCasa%20AloHa%20152%20Alojamiento%20Habitacional!5e0!3m2!1sen!2sco!4v1698453773716!5m2!1sen!2sco" loading="lazy" width={1000} height={700} referrerPolicy="no-referrer-when-downgrade"></iframe></div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar () {
                 <div className='collapse navbar-collapse' id="navbarTogglerDemo01"> 
                     <ul className="navbarFV navbar-nav mx-auto mb-2 mb-lg-0 nav-justified">
                         <li>
-                        <NavLink to="/"><img src="/imgRooms/LogoAloHaEnBlanco.svg" alt="Logo" className='logo' style={{width:"80px" , height:"80px"}}/></NavLink>
+                        <NavLink to="/"><img src="/imgRooms/LogoAloHaEnBlanco.svg" alt="Logo" className='logo' /></NavLink>
                         </li>
                         <li className="dropdown itemList tres">
                             <button className="dropbtn"><NavLink to="/habitaciones" className="itemListLink">Habitaciones ▼</NavLink></button>
