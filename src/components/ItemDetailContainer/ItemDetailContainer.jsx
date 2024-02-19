aa488d67b7fafc1ae2db641e51bb64d7bcbd8353
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2024-02-19 15:27:07 -0600

    Se agrego el precio por noche y el tiempo minimo de estadia , falta agregar los precios en la base de datos

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -50,7 +50,9 @@ if (room) {
         <div className="card-detail_detail">
           <h1 className="ItemDetailTitle">{room.title}</h1>
           <p className="description">{room.description}</p>
-          <h3>$ {(room.price)} Cop / Mes</h3>
+          <h3> Desde $ {room.price} Cop / Mes</h3>
+          <h4>Desde $ {room.price} Cop / Noche</h4>
+          <p style={{color:"darkgray"}}>*Estadia Minima 3 noches*</p>
           <a href="https://api.whatsapp.com/send?phone=573212402566&text=¡%20Hola%20Casa%20AloHa%20152%20!%20Estoy%20interesado%20en%20conocer%20más%20información%20sobre%20el%20hospedaje" target="_blank" rel="noreferrer"  className="WhatsappButton"><Icon icon="ic:baseline-whatsapp"  width={50} height={50}/>Reserva por Whatsapp</a>
         </div>
       </div>
