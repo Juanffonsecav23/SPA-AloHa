2aad5f4fa4cca5247a23609312efd19bc4690125
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2024-02-19 15:59:21 -0600

    Se agregaron los precios por noche y el mensaje de estadia minima , tambien se quito el boton de ver mas detalles y se vambio el titulo de los items

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,17 +4,18 @@ import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 
 
-function CardDescription({ price, category}) {
+function CardDescription({ price, category, priceNight}) {
   
   return (
     <div className="item-card_detail">
-      <h5 className="item-card_price-tag">$ {price}  COP / Mes</h5>
+      <h5 className="item-card_price-tag">$ Desde {price}  COP / Mes</h5>
+      <p className="item-card_price-tag">$ Desde {priceNight}  COP / Noche</p>
       <small>{category}</small>
     </div>
   );
 }
 
-function Item({ title, img, price, category, id, capacidad }) {
+function Item({ title, img, price, category, id, capacidad,priceNight }) {
   const stylesButton = {
     backgroundColor: capacidad === 0 ? "grey" : "inherit",
   };
@@ -26,10 +27,10 @@ function Item({ title, img, price, category, id, capacidad }) {
       <img src={img} alt="Imagen Habitacion"></img>
       </div>
       <div className="item-card_header">
-        <h5 style={{marginLeft: "5px"}}>{title}</h5> 
+        <h5 style={{marginLeft: "5px", color:"orange"}}>{title}</h5> 
         <small style={{marginLeft: "5px"}}>{category}</small>
-        <CardDescription price={(price)}   />
-        <Button className="btn-ver-detalles" style={stylesButton}>Ver detalles</Button> 
+        <CardDescription price={(price)} priceNight={(priceNight)}  />
+        <p style={{color:"darkgray"}}>*Estadía mínima 3 noches*</p>
       </div>
     </div>
     </Link >
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,5 @@
 import {  useEffect, useState } from "react";
 import "./ItemDetail.css";
-
 import {  useParams, } from "react-router-dom";
 import Loader from "../Loader/Loader";
 import { getRoomData } from "../../services/Firebase";
@@ -51,8 +50,8 @@ if (room) {
           <h1 className="ItemDetailTitle">{room.title}</h1>
           <p className="description">{room.description}</p>
           <h3> Desde $ {room.price} Cop / Mes</h3>
-          <h4>Desde $ {room.price} Cop / Noche</h4>
-          <p style={{color:"darkgray"}}>*Estadia Minima 3 noches*</p>
+          <h4>Desde $ {room.priceNight} Cop / Noche</h4>
+          <p style={{color:"darkgray"}}>*Estadía Mínima 3 noches*</p>
           <a href="https://api.whatsapp.com/send?phone=573212402566&text=¡%20Hola%20Casa%20AloHa%20152%20!%20Estoy%20interesado%20en%20conocer%20más%20información%20sobre%20el%20hospedaje" target="_blank" rel="noreferrer"  className="WhatsappButton"><Icon icon="ic:baseline-whatsapp"  width={50} height={50}/>Reserva por Whatsapp</a>
         </div>
       </div>
